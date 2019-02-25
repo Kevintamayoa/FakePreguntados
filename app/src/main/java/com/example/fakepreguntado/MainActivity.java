9a10cfefd9d76ede00ed24c5f460fee2e6bdabf1
Kevin Tamayo <kelpo_tamayo@hotmail.com>
2019-02-24 20:29:55 -0600

    Vista landscape de inicio,empecé config

--- app/src/main/java/com/example/fakepreguntado/MainActivity.java
+++ app/src/main/java/com/example/fakepreguntado/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.fakepreguntado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Configbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Configbtn=findViewById(R.id.config_btn);
+        Configbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent config=new Intent(MainActivity.this,form_opciones.class);
+                startActivity(config);
+            }
+        });
     }
 }

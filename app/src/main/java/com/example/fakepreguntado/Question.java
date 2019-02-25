1738d4cbe6f328f5d073defd0c6ce56c4f81b380
Rafa <ralfgp95@gmail.com>
2019-02-25 15:33:46 -0600

    Elimine el activity cheat y acomplete la clase Question

--- app/src/main/java/com/example/fakepreguntado/Question.java
+++ app/src/main/java/com/example/fakepreguntado/Question.java
@@ -3,13 +3,21 @@ package com.example.fakepreguntado;
 public class Question {
     private int id;
     private String pregunta;
+    private boolean status;
     private boolean respuesta;
+    private boolean cheat;
+    private int dificultad;
+    private int categoria;
     private int puntaje;
 
-    public Question(int id, String pregunta, boolean respuesta, int puntaje) {
+    public Question(int id, String pregunta, boolean status, boolean respuesta, boolean cheat, int dificultad, int categoria, int puntaje) {
         this.id = id;
         this.pregunta = pregunta;
+        this.status = status;
         this.respuesta = respuesta;
+        this.cheat = cheat;
+        this.dificultad = dificultad;
+        this.categoria = categoria;
         this.puntaje = puntaje;
     }
 
@@ -17,7 +25,21 @@ public class Question {
 
     public String getQuestion(){ return pregunta;}
 
-    public boolean getAnswer(){ return respuesta;}
+    public boolean getStatus(){return status;}
+
+    public void setStatus(){status=true;}
+
+    public boolean getAnswer(){return respuesta;}
+
+    public boolean getCheat(){return cheat;}
+
+    public void setCheat(){cheat=true;}
+
+    public int getDifficulty() {return dificultad;}
+
+    public int getCategory(){return categoria;}
 
     public int getPoints(){return puntaje;}
+
+
 }

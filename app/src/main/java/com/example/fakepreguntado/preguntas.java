d73efb717077d42744e5c67902499ba02dec7ca6
Rafa <ralfgp95@gmail.com>
2019-02-24 23:22:37 -0600

    Cambios en mis xmls y empece mis javas

--- app/src/main/java/com/example/fakepreguntado/preguntas.java
+++ app/src/main/java/com/example/fakepreguntado/preguntas.java
@@ -2,12 +2,76 @@ package com.example.fakepreguntado;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class preguntas extends AppCompatActivity {
+
+    TextView numPregunta;
+    Button cheatActivityButton;
+    ImageButton pregAnterior;
+    ImageButton pregSiguiente;
+    TextView textoPregunta;
+    ImageButton correctButton;
+    ImageButton incorrectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+
+        numPregunta = findViewById(R.id.num_pregunta);
+        cheatActivityButton = findViewById(R.id.cheat_activity_button);
+        pregAnterior = findViewById(R.id.ant_pregunta);
+        pregSiguiente = findViewById(R.id.sig_pregunta);
+        textoPregunta = findViewById(R.id.texto_pregunta);
+        correctButton = findViewById(R.id.correct_button);
+        incorrectButton = findViewById(R.id.incorrect_button);
+
+        numPregunta.setText("0/0");
+
+        cheatActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        pregAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        pregSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        textoPregunta.setText("P");
+
+        correctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        incorrectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/example/fakepreguntado/trophies.java
+++ app/src/main/java/com/example/fakepreguntado/trophies.java
@@ -1,13 +1,38 @@
 package com.example.fakepreguntado;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class trophies extends AppCompatActivity {
+
+    ImageView endTrophy;
+    TextView primerLugar;
+    TextView segundoLugar;
+    TextView tercerLugar;
+    TextView cuartoLugar;
+    TextView quintoLugar;
+    TextView sextoLugar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophies);
+
+        endTrophy = findViewById(R.id.image_trophies);
+        primerLugar = findViewById(R.id.primer_lugar);
+        segundoLugar = findViewById(R.id.segundo_lugar);
+        tercerLugar = findViewById(R.id.tercer_lugar);
+        cuartoLugar = findViewById(R.id.cuarto_lugar);
+        quintoLugar = findViewById(R.id.quinto_lugar);
+        sextoLugar = findViewById(R.id.sexto_lugar);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }

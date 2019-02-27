31cc31723a06642c7e30d5f03524468c309049ee
Rafa <ralfgp95@gmail.com>
2019-02-26 21:46:23 -0600

    Habilite el boton play del menu principal, tan solo para comprobar la funcion de la actividad preguntas

--- app/src/main/java/com/example/fakepreguntado/MainActivity.java
+++ app/src/main/java/com/example/fakepreguntado/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button Configbtn;
+    Button Playbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent config=new Intent(MainActivity.this,form_opciones.class);
                 startActivity(config);
+            }
+        });
+
+        Playbtn=findViewById(R.id.jugar_btn);
+        Playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play = new Intent(MainActivity.this,PreguntasActivity.class);
+                startActivity(play);
             }
         });
     }

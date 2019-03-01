5798bc486322e59e1cb6a53a971c1bc56bb58336
Rafa <ralfgp95@gmail.com>
2019-03-01 01:49:52 -0600

    Listo la funcionalidad de trofeos, namas falta que se pase la el puntaje y si se hizo trampa

--- app/src/main/java/com/example/fakepreguntado/TrofeosActivity.java
+++ app/src/main/java/com/example/fakepreguntado/TrofeosActivity.java
@@ -3,6 +3,7 @@ package com.example.fakepreguntado;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,7 +30,8 @@ public class TrofeosActivity extends AppCompatActivity {
 
     TrofeosActivityModel trofeosActivityModel;
 
-    public String playerName="";
+    public String playerName = "";
+    public int Puntaje = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -49,29 +51,29 @@ public class TrofeosActivity extends AppCompatActivity {
         final NumberPicker letras = new NumberPicker(TrofeosActivity.this);
         letras.setMaxValue(25);
         letras.setMinValue(0);
-        letras.setDisplayedValues(new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N"
-                ,"O","P","Q","R","S","T","U","V","W","X","Y","Z"});
+        letras.setDisplayedValues(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
+                , "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"});
 
         final NumberPicker letras2 = new NumberPicker(TrofeosActivity.this);
         letras2.setMaxValue(25);
         letras2.setMinValue(0);
-        letras2.setDisplayedValues(new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N"
-                ,"O","P","Q","R","S","T","U","V","W","X","Y","Z"});
+        letras2.setDisplayedValues(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
+                , "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"});
 
         final NumberPicker letras3 = new NumberPicker(TrofeosActivity.this);
         letras3.setMaxValue(25);
         letras3.setMinValue(0);
-        letras3.setDisplayedValues(new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N"
-                ,"O","P","Q","R","S","T","U","V","W","X","Y","Z"});
+        letras3.setDisplayedValues(new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
+                , "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"});
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(50, 50);
-        params.gravity= Gravity.CENTER;
+        params.gravity = Gravity.CENTER;
 
         LinearLayout.LayoutParams letrasparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        letrasparams.weight=1;
+        letrasparams.weight = 1;
         LL.setLayoutParams(params);
-        LL.addView(letras,letrasparams);
-        LL.addView(letras2,letrasparams);
-        LL.addView(letras3,letrasparams);
+        LL.addView(letras, letrasparams);
+        LL.addView(letras2, letrasparams);
+        LL.addView(letras3, letrasparams);
 
         AlertDialog.Builder nameChooser = new AlertDialog.Builder(TrofeosActivity.this);
         nameChooser.setTitle("JUGADOR");
@@ -81,21 +83,27 @@ public class TrofeosActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 trofeosActivityModel = ViewModelProviders.of(TrofeosActivity.this).get(TrofeosActivityModel.class);
-                playerName=passNameFromDialog(letras.getValue(),letras2.getValue(),letras3.getValue());
-                trofeosActivityModel.loadHonestScores(playerName,70,false);
-                primerLugar.setText("1er Lugar: "+trofeosActivityModel.getHonestUserName(0));
-                segundoLugar.setText("2do Lugar: "+trofeosActivityModel.getHonestUserName(1));
-                tercerLugar.setText("3er Lugar: "+trofeosActivityModel.getHonestUserName(2));
+                playerName = passNameFromDialog(letras.getValue(), letras2.getValue(), letras3.getValue());
+                trofeosActivityModel.loadHonestScores(playerName, 70, false);
+                primerLugar.setText("1er Lugar: " + trofeosActivityModel.getHonestUserName(0));
+                segundoLugar.setText("2do Lugar: " + trofeosActivityModel.getHonestUserName(1));
+                tercerLugar.setText("3er Lugar: " + trofeosActivityModel.getHonestUserName(2));
                 trofeosActivityModel.loadMixedScores(trofeosActivityModel.highestHonestScores.get(0),
                         trofeosActivityModel.highestHonestScores.get(1),
                         trofeosActivityModel.highestHonestScores.get(2));
-                cuartoLugar.setText("4to Lugar: "+trofeosActivityModel.highestMixedScores.get(0).getName());
-                quintoLugar.setText("5to Lugar: "+trofeosActivityModel.highestMixedScores.get(1).getName());
-                sextoLugar.setText("6to Lugar: "+trofeosActivityModel.highestMixedScores.get(2).getName());
+                cuartoLugar.setText("4to Lugar: " + trofeosActivityModel.highestMixedScores.get(0).getName());
+                quintoLugar.setText("5to Lugar: " + trofeosActivityModel.highestMixedScores.get(1).getName());
+                sextoLugar.setText("6to Lugar: " + trofeosActivityModel.highestMixedScores.get(2).getName());
             }
         });
         nameChooser.show();
-        //endTrophy.setImageTintList();
+        if (Puntaje < 25) {
+            endTrophy.setColorFilter(Color.rgb(105, 105, 105));
+        } else if (Puntaje >= 25 && Puntaje < 50) {
+            endTrophy.setColorFilter(Color.rgb(218, 165, 32));
+        } else if (Puntaje > 50) {
+            endTrophy.setColorFilter(Color.rgb(255, 215, 0));
+        }
     }
 
     @Override
@@ -103,7 +111,8 @@ public class TrofeosActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
-    public String passNameFromDialog(int s1,int s2,int s3){
-        return Character.toString((char)(s1+65))+Character.toString((char)(s2+65))+Character.toString((char)(s3+65));
+    public String passNameFromDialog(int s1, int s2, int s3) {
+        return Character.toString((char) (s1 + 65)) + Character.toString((char) (s2 + 65)) + Character.toString((char) (s3 + 65));
     }
 }
+
